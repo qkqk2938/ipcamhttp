aea533d2016c151184eda1445843f85b2bc5b44b
wonsang <qkqk2938@naver.com>
2023-04-18 08:05:14 +0000

    3client

--- module/websoket.js
+++ module/websoket.js
@@ -4,8 +4,9 @@ const wsModule = require('ws');
 class Websocket {
     
     constructor() {
-        this.ipcam;
-        this.mon;
+        this.ipcam = null;
+        this.drivetrain = null;
+        this.mon = null;
     }
     
 
@@ -25,32 +26,31 @@ class Websocket {
             const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
             console.log(`새로운 클라이언트[${ip}] 접속`);
             const conAuth = request.headers['sec-websocket-protocol'];
-            const conAuthArr = conAuth.split('_');
-            if(conAuthArr[0]=="mon" && conAuthArr[1] == "2938" && this.mon ===undefined){
+            if(conAuth=="mon_2938"){
                 this.mon = ws;
                 console.log(`mon up`);
-                if(this.ipcam !==undefined){
+                if(this.ipcam){
                     console.log(`Live Start`);
                     this.ipcam.send("{'data':'liveStart'}");
                 }
                 ws.on('message', (msg)=>{
-                    if(this.ipcam !==undefined){ 
-                        this.ipcam.send(msg);
+                    if(this.drivetrain && this.ipcam){ 
+                        this.drivetrain.send(msg);
                     }
                     
                 })
                 // 연결 종료 이벤트 처리
                 ws.on('close', ()=>{
                     console.log(`클라이언트[${ip}] 웹소켓 연결 종료`);
-                    this.mon = undefined;                
+                    this.mon = null;                
                     console.log(`mon die`);
                 
                 })
-            }else if(conAuthArr[0]=="ipcam" && this.ipcam ===undefined){
+            }else if(conAuth=="ipcam"){
                 this.ipcam = ws; 
                 console.log(`ipcam up`);
                 ws.on('message', (msg)=>{
-                    if(this.mon !==undefined){                        
+                    if(this.mon){                        
                         this.mon.send(msg);
                     }else{
                         console.log(`Live Stop`);
@@ -61,8 +61,21 @@ class Websocket {
                  // 연결 종료 이벤트 처리
                 ws.on('close', ()=>{
                     console.log(`클라이언트[${ip}] 웹소켓 연결 종료`);
-                    this.ipcam = undefined;                
+                    this.ipcam = null;                
                     console.log(`ipcam die`);
+                    
+                })
+            }else if(conAuth=="drivetrain"){
+                this.drivetrain = ws; 
+                console.log(`drivetrain up`);
+                ws.on('message', (msg)=>{
+                                    
+                })
+                 // 연결 종료 이벤트 처리
+                ws.on('close', ()=>{
+                    console.log(`클라이언트[${ip}] 웹소켓 연결 종료`);
+                    this.drivetrain = null;                
+                    console.log(`drivetrain die`);
                     
                 })
             }else{
